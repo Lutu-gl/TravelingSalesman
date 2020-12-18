57c5aaf867c0ebe78d6338c30a2f6fa7b156cb44
Lukas-Siller <sillerlukas4mail.com>
2020-12-18 12:30:14 +0100

    test veraenderung 2

--- travelingsalesman/Main.java
+++ travelingsalesman/Main.java
@@ -3,6 +3,6 @@ package travelingsalesman;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Traveling Salesman");
-        System.out.println();
+        System.out.println("Test");
     }
 }
