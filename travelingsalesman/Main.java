3e1bb24a0391ce4571344223347caf6f66edd432
Lukas-Siller <sillerlukas4mail.com>
2020-12-18 12:32:22 +0100

    test

--- travelingsalesman/Main.java
+++ travelingsalesman/Main.java
@@ -3,6 +3,5 @@ package travelingsalesman;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Traveling Salesman");
-        System.out.println("Test");
     }
 }
