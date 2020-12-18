196ad33f46bc752dbbd66b51ac8dadd9736b9a58
Lukas-Siller <sillerlukas4mail.com>
2020-12-18 12:37:55 +0100

    test

--- travelingsalesman/Main.java
+++ travelingsalesman/Main.java
@@ -2,6 +2,6 @@ package travelingsalesman;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Traveling Salesman Test1");
+        System.out.println("Traveling Salesman Test12");
     }
 }
