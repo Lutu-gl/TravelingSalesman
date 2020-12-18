f22563d44625e1f11b06260a7dbed5778d70d773
chris <christofhochrainer@gmail.com>
2020-12-18 12:36:43 +0100

    Test3

--- travelingsalesman/Main.java
+++ travelingsalesman/Main.java
@@ -2,6 +2,6 @@ package travelingsalesman;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Traveling Salesman Test");
+        System.out.println("Traveling Salesman Test1");
     }
 }
