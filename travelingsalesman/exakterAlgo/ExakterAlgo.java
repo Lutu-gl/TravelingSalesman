1c1cc7394dc098482104a4ae2ce6cfed887fdef0
Lutu <luca.dris@web.de>
2020-12-28 15:29:44 +0100

    ExakterAlgo Branch gemacht und File erstellt

---
+++ travelingsalesman/exakterAlgo/ExakterAlgo.java
@@ -0,0 +1,8 @@
+package travelingsalesman.exakterAlgo;
+
+
+//Wahl eines exakten Algorithmus und beispielhafte Anwendung an einem kleinen Beispiel (Implementation in Java)
+public class ExakterAlgo {
+
+
+}
