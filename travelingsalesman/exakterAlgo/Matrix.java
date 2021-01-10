b5c6b85ea2230f7a6f78353a698507b8a62c488f
Lutu <luca.dris@web.de>
2021-01-10 11:28:22 +0100

    Bei Matrix.java matrix hinzugefügt

--- travelingsalesman/exakterAlgo/Matrix.java
+++ travelingsalesman/exakterAlgo/Matrix.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public final class Matrix {
-    //private double[][] matrix = new double[length][length];
+    private double[][] matrix = new double[1000][1000];
 
 
     public Matrix(){
@@ -14,6 +14,8 @@ public final class Matrix {
 
     public boolean readMatrixFromFile(String name){
         String[] spitData;
+        int row = 0;        //Zeile
+        int column = 0;     //Spalte
 
         try {
             File file = new File(name);
@@ -22,18 +24,22 @@ public final class Matrix {
             while(reader.hasNextLine()){
                 String data = reader.nextLine();
                 spitData = data.split(",");
+
                 for(String ausgabe : spitData){
-                    System.out.print(ausgabe + " ");
+                    System.out.print(Double.parseDouble(ausgabe) + " ");
+                    matrix[row][column] = Double.parseDouble(ausgabe);
+                    column++;
                 }
                 System.out.println();
+                row++;
+                column=0;
             }
 
+            reader.close();
         } catch(FileNotFoundException e){
             e.printStackTrace();
+            return false;
         }
-
-
-
 
 
         return true;
