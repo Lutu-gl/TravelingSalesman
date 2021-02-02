9fb4cc244dbc135e7b0eb4a5202a1f3b334d1559
Lutu_gl <luca.dris@web.de>
2021-02-02 22:08:34 +0100

    Ort mit Javadoc gemacht

--- travelingsalesman/heuristischerAlgo/Ort.java
+++ travelingsalesman/heuristischerAlgo/Ort.java
@@ -1,19 +1,35 @@
 package travelingsalesman.heuristischerAlgo;
 
-
+/**
+ * @author Lutu
+ * Das ist die Klasse ort, die ein index speichert.
+ * Dieser Index gibt an welchem Block/Zeile in der Matrix dieser Ort belegt
+ */
 public class Ort {
     private int index;
 
     public Ort(){}
 
+    /**
+     * @param index
+     * ein weiterer Konstruktor.
+     * */
     public Ort(int index) {
         this.index = index;
     }
 
-
+    /**
+     * @return index an welchem Block/Zeile in der Matrix dieser Ort belegt
+     *  Getter
+     */
     public int getIndex() {
         return index;
     }
+
+    /**
+     * @param index an welchem
+     * Setter
+     */
     public void setIndex(int index) {
         this.index = index;
     }
